ce38ff8a8c3d449e435e88533e9a594b105ad046
Jo <77723880+joawesome@users.noreply.github.com>
2021-08-30 05:21:46 -0400

    added files

---
+++ btAudioInternal/examples/advertiseBluetooth/advertiseBluetooth.ino
@@ -0,0 +1,15 @@
+#include <btAudioInternal.h>
+
+// Sets the name of the audio device
+btAudio audio = btAudio("ESP_Speaker");
+
+void setup() {
+ 
+ // streams audio data to the ESP32   
+ audio.begin();
+
+}
+
+void loop() {
+
+}
